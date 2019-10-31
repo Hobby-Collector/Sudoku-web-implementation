26c6eeedfe3f0cd808c2594c2dd79e8b2951a2fb
BlaineShelton <mapleteer@gmail.com>
2019-10-31 11:43:24 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*----- constants -----*/
+//array of arrays of sudoku boards
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+//text changed listener on each board El
+
+//input lost focus listener
+
+//difficulty buttons listener
+
+/*----- functions -----*/
+
+//init function
+//clear the board and reset the message
+
+//input changed listener function
+//if text is entered and it is a number 
+//replace the previous digit input to the new digit
+
+//input focus lost listener function
+//if digit entered does not match the 'correct' digit in the solution arr set border to red 
+//otherwise make it green
+
+//difficulty button listener function
+//pull a random puzzle from the array of the related id name
+
+//render function
+//if all board spot have text compare to the solution highlight incorrect answers or display winner message
+
